7ba9b9feb766349a8174888bd57dd4a83faf56c6
MatheusFelipeSouzaSantana <matheussantana9868@gmail.com>
2024-05-22 12:52:14 -0300

    Criação model contact, e do personalização Django admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'firts_name', 'last_name', 'phone',
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name',
