8bfa06cc02c297c230b85626eab6f0c982ab86bb
MatheusFelipeSouzaSantana <matheussantana9868@gmail.com>
2024-05-24 13:32:05 -0300

    Adicionado categorias aos contatos

--- contact/admin.py
+++ contact/admin.py
@@ -6,3 +6,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'firts_name', 'last_name', 'phone',
     ordering = '-id',
     search_fields = 'id', 'firts_name', 'last_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Contact(models.Model):
     firts_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True) 
@@ -10,6 +16,7 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.firts_name} {self.last_name}'
